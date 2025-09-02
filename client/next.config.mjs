89836333cff136780864120caf155e1b868506f3
parkeaseofficial4-netizen <parkease.official.4@gmail.com>
2025-09-02 13:48:02 +0600

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://park-ease-web-final.vercel.app/api/:path*", // your backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;
